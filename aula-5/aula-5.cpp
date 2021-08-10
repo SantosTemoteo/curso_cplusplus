9cd8cfa895438d8c17ed2c7a89b247d568717643
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-09 23:48:26 -0300

    Declaraçẽs de variáveis Multiplas

---
+++ aula-5/aula-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "pode-se associar comandos \n\n";   // pode-se associar comandos
+
+int main(){
+
+    int vidas=3, tiros=500, life=100;   // declaração de variável multipla    
+    
+    cout << pi << "\n\n";
+    canal
+
+
+    return 0;
+}
