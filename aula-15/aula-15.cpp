f99965cb7a754dc8e74045d046d8ffe948e0fe61
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-12 22:47:00 -0300

    Comando break;

---
+++ aula-15/aula-15.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int cont;
+
+    cont = 0;
+
+    while(cont <= 100){
+        cout << cont << "\n";
+        if(cont == 50){
+            break;
+        }
+        cont++; // precisa por em baixo para que o 50 seja impresso.
+    }
+
+
+
+
+
+
+    return 0;
+}
