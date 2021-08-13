29365d456e7aba819b46f9d7b64b2507a41d247e
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-12 23:56:23 -0300

    Comando Do While

---
+++ aula-16/aula-16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int cont;
+    cont = 0;
+
+    do{
+        cout << cont << "\n";
+        
+    } while(cont++ < 20);
+
+    cout << "Fim.";
+
+    return 0;
+}
