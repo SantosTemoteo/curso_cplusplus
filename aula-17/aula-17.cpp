b6a30f4cd58264a7e6bcffc041e88e794b762a44
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-13 00:47:32 -0300

    FOR Loop

---
+++ aula-17/aula-17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x,y,z; // pode inicializar mais de uma variável
+
+    for(x=0,y=1,z=0; x<=10 && z<=6; x++,y+=2,z+=2){
+        cout << x << "-";
+        cout << y << "-";
+        cout << z << "\n";
+    }
+
+    return 0;
+}
