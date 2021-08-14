d8a418ca54373309dffaff97c792241ddcd3a4eb
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-14 19:38:11 -0300

    Difference

---
+++ exercicios/ex6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+    // Difference
+    int main(){
+
+        int A,B,C,D,PRODUCT;
+
+        cin >> A >> B >> C >> D;
+        PRODUCT = A*B - C*D;
+
+        cout << "DIFERENCA = " << PRODUCT << endl;
+
+    return 0;
+    }
