499b34daab3c9f1f6ae78ddc7cd27ac3a87dbfec
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-14 02:40:50 -0300

    Simple Product

---
+++ exercicios/ex3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int PROD,A,B;
+
+    cin >> A;
+    cin >> B;
+
+    PROD = A*B;
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
+
+
+//Simple Product
