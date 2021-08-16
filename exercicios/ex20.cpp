ed08f12dd6fddfa07ff61d174b61a52a79b7a3a1
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-16 19:57:10 -0300

    URI 1020 Age in Days

---
+++ exercicios/ex20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int value, ano, mes;
+    cin >> value;
+
+    ano = value/365;
+    value = value%365;
+
+    mes = value / 30;
+    value = value % 30;
+
+    cout << ano << " ano(s)" << endl;
+    cout << mes << " mes(es)" << endl;
+    cout << value << " dias(s)" << endl;
+
+    return 0;
+}
