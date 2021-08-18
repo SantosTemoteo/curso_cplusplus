08101a0235cfb03686b667436e26129ed462d0a5
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-18 19:50:03 -0300

    URI 1036 - Bhaskara's Formula

--- exercicios/ex23.cpp
+++ exercicios/ex23.cpp
@@ -4,16 +4,17 @@ using namespace std;
 // Bhaskara's Formula
 int main(){
 
-    double a, b, c, rootdelta, x1, x2;
+    double a, b, c, rootdelta, delta, x1, x2;
     
     cin >> a >>  b >> c;
 
-    rootdelta = sqrt((b*b)-4*a*c);
+    delta = (b*b)-4*a*c;
+    rootdelta = sqrt(delta);
     
-    x1 = (b*-1+rootdelta)/2.0*a;
-    x2 = (b*-1-rootdelta)/2.0*a;
+    x1 = ((b*-1)+rootdelta)/(2*a);
+    x2 = ((b*-1)-rootdelta)/(2*a);
     
-    if(a == 0 || b == 0 || c == 0 || rootdelta < 0){
+    if(a == 0 || b == 0 || c == 0 || delta < 0){
         cout << "impossivel calcular" << endl;
     }else{
     cout.precision(5);
