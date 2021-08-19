8ce0cb948bbab3ab4a57c40df9c6a2fffae7c1d0
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-19 02:32:09 -0300

    Averege 3

--- exercicios/beginner/uri1040.cpp
+++ exercicios/beginner/uri1040.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 using namespace std;
 
+        // Average 3
 int main(){
-
     
+    double n1, n2, n3, n4, media;
+    cin >> n1 >> n2 >> n3 >> n4;
 
+    media = (n1*2 + n2*3 + n3*4 + n4*1)/10;    // 2,3,4,1 respectively weights
+    
+    cout.precision(1);
+    cout << std::fixed << "Media: " << media << endl; 
+   
+    if(media >= 7){
+        cout << "Aluno aprovado" << endl;
+    }
+    if(media < 5){
+        cout << "Aluno reprovado." << endl;
+    }
+    if(media >= 5 && media <= 6.9){
+        cout << "Aluno em Exame." << endl;
+        cout << "Nota do exame: ";
+        cin >> n1;
+        media = (media+n1)/2;
+
+            if(media >= 5){
+                cout << "Aluno aprovado." << endl;
+            }else{
+                cout << "Aluno reprovado." << endl;
+            }
+
+        cout << std::fixed << "Media final: " << media << endl;
+
+    }
 
     return 0;
 }
