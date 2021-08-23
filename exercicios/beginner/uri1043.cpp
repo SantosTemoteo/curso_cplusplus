92c58687d32398f5aba6f7ef835da7eb06ace5cb
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-23 16:56:53 -0300

    Triangle

--- exercicios/beginner/uri1043.cpp
+++ exercicios/beginner/uri1043.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
-        //triangle
 
+        //triangle
 int main(){
 
-    
+    double a,b,c,per,area;
+    cin >> a >> b >> c;
+
+    cout.precision(1);
+    if(a+b > c && a+c > b && b+c > a){
+
+        per = a+b+c;
+        cout << std::fixed << "Perimetro = " << per << endl;
+    }else{
+        area =((a+b)*c)/2;
+        cout << std::fixed << "Area = " << area << endl;
+    }
 
     return 0;
 }
