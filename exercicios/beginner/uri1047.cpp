431ae79e88024fd5d0ea5758192b22929ee8e6ed
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-25 04:37:35 -0300

    Game Time with Minutes

---
+++ exercicios/beginner/uri1047.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int initial_hour, initial_minute, final_hour, final_minute, r_hour, r_min;
+    cin >> initial_hour >> initial_minute >> final_hour >> final_minute;
+
+    r_hour = final_hour - initial_hour;
+    r_min = final_minute - initial_minute;      // para os minutos iremos ter uma consideração parecida, só que com 60
+
+    if(r_hour < 0){     // hora negativa, ou seja, passou-se um dia.
+        r_hour += 24;
+    }
+    if(r_min < 0){
+        r_min += 60;    // incrementa 60 min 
+        r_hour -= 1;     // e dencrementa 1h
+    }
+    if(r_min == 0 && r_hour == 0){
+        cout << "O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)" << endl;
+    }else{
+        cout << "O JOGO DUROU " << r_hour << " HORA(S) E " << r_min << " MINUTO(s)" << endl;
+    }                    
+    return 0;
+}
