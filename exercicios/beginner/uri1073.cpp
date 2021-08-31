0a3f2e31ca59fbb2ad62271ea42fd77d1d1085d1
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-31 01:29:10 -0300

    Even Square

---
+++ exercicios/beginner/uri1073.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n,i,square;
+    cin >> n;
+ 
+    for(i = 1; i <= n; i++){
+        if(i%2 == 0){    
+            square = i*i;
+            cout << i << "^" << 2 << " = " << square << endl;
+        }
+    }
+
+    return 0;
+}
