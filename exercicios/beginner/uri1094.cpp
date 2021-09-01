43e531dacd058936666378bbbccf7a912edee048
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-09-01 18:35:56 -0300

    Experiments

--- exercicios/beginner/uri1094.cpp
+++ exercicios/beginner/uri1094.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(){
 
-    int n,i,x,ratos = 0,sapos = 0,coelhos = 0,cobaias = 0;
+    int n,i,x;
+    double ratos = 0,sapos = 0,coelhos = 0,cobaias = 0;
     double p_coelhos,p_ratos,p_sapos;
     char j[2];
     
@@ -18,11 +19,18 @@ int main(){
             sapos += x;
         }
         cobaias = coelhos + ratos + sapos;
+        p_coelhos = (coelhos/cobaias)*100;
+        p_ratos = (ratos/cobaias)*100;
+        p_sapos = (sapos/cobaias)*100;
     }
     cout << "Total: " << cobaias << " cobaias" << endl;
     cout << "Total de coelhos: " << coelhos << endl;
     cout << "Total de ratos: " << ratos << endl;
     cout << "Total de sapos: " << sapos << endl;
+    cout.precision(2);
+    cout << std::fixed << "Percentual de coelhos: " << p_coelhos << " %" << endl;
+    cout << std::fixed << "Percentual de ratos: " << p_ratos << " %" << endl;
+    cout << std::fixed << "Percentual de sapos: " << p_sapos << " %" << endl;
     
     return 0;
 }
