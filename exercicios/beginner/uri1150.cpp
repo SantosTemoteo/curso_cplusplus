a3d20d9b30244f2d8f5be1346c4c851a97f87e26
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-09-11 17:44:14 -0300

    Exceeding Z

--- exercicios/beginner/uri1150.cpp
+++ exercicios/beginner/uri1150.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main(){     // 100% COPIADO
 
-    int x,z,a=1,c=0,i;
+    int x,z,a=1,c=0,i;          // a representa o montante dos números na soma até ultrapassar Z.
     cin >> x;
     cin >> z;
 
     while(z<=x){
-        cin >>z;
+        cin >>z;            // quando z>x, a ja irá contar 1.
     }
     for(i=x;i<=z;i++){
-        c+=i;
+        c+=i;               // contagem validadora para o loop abaixo
         if(c>z){
             break;
         }
-        a++;
+        a++;                // a=1, soma-se a++;
     }
-    cout << a << endl;
+    cout << a << endl;      // final printa o montande até ultrapassar Z
     return 0;
 }
