47c57b08ab4a7d0516a6b2e18e77d32dee1b1e33
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-09-10 23:11:29 -0300

    Logical Sequence 2

--- exercicios/beginner/uri1145.cpp
+++ exercicios/beginner/uri1145.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){     // 75% COPIADO >:(
+int main(){     // 75% COPIADO, presentation error 100% >:(
 
-    int x,y,aux,i;
+    int x,y,aux=0,i;
     cin >> x >> y;
 
-    for(i=1;i<=y;++i){
+    for(i=1;i<=y;i++){
         cout << i << " ";
         aux+=1;
         if(aux==x){
-            cout << "" << endl;
+            cout << endl;
             aux=0;
         }
+        if(i==y){break;}
     }
     return 0;
 }
