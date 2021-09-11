d636e3938abf24a92f344c0831cf44d22269a746
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-09-11 01:16:39 -0300

    :(

--- exercicios/beginner/uri1145.cpp
+++ exercicios/beginner/uri1145.cpp
@@ -5,15 +5,19 @@ int main(){     // 75% COPIADO, presentation error 100% >:(
 
     int x,y,aux=0,i;
     cin >> x >> y;
-
+    y-=1;
     for(i=1;i<=y;i++){
         cout << i << " ";
+        if(i==y){
+            cout << i << endl;
+        }
         aux+=1;
         if(aux==x){
             cout << endl;
             aux=0;
         }
-        if(i==y){break;}
+        
+        if(i==y+1){break;}
     }
     return 0;
 }
